5aefa7436352abb459648c7c14cdbeed3833b518
Rafael Cesar <rafael_planseq@hotmail.com>
2026-06-02 18:06:06 -0300

    Aprendendo sobre logica sequencial

---
+++ Java/01-EstruturaSequenciais/src/LogicaSequencial.java
@@ -0,0 +1,8 @@
+public class LogicaSequencial {
+    static void main(String[] args) {
+        System.out.println("instrução numero 1");
+        System.out.println("instrução numero 2");
+        System.out.println("instrução numero 3");
+        System.out.println("instrução numero 4");
+    }
+}
