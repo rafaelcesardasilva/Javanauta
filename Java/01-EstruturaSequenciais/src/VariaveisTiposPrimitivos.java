118a198cbf6318437275c3135295aeca5a78adb7
Rafael Cesar <rafael_planseq@hotmail.com>
2026-06-02 21:11:02 -0300

    Aprendendo sobre Variaveis e Tipos primitivos

---
+++ Java/01-EstruturaSequenciais/src/VariaveisTiposPrimitivos.java
@@ -0,0 +1,23 @@
+
+/*
+Inteiros: byte, short, int, long
+Pontos Flutuente: float, double
+Caractere: char (um único caractere Unicode)
+Booleano: boolean (Representa verdadeiro ou falso)
+
+ */
+public class VariaveisTiposPrimitivos {
+    static void main(String[] args) {
+        int idade = 30;
+        long quantidadeCachorros = 10L;
+        double salarioMensal = 4500.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
+
+        System.out.println("idade: " + idade);
+        System.out.println("Salario: " + salarioMensal);
+        System.out.println("Genero: " + genero);
+        System.out.println("Empregado Java: " + empregadoJava);
+        System.out.println("Quantidade: " + quantidadeCachorros);
+    }
+}
