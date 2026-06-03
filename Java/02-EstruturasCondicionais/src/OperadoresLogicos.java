98d5b97c9909d54843f7b152889ef2514180ddc2
Rafael Cesar <rafael_planseq@hotmail.com>
2026-06-02 22:47:55 -0300

    Aprendendo sobre Operadores Logicos

---
+++ Java/02-EstruturasCondicionais/src/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+/*
+&& , ||  ,  !
+ */
+public class OperadoresLogicos {
+    static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x && y);
+        System.out.println(x || y);
+        System.out.println(!x);
+    }
+}
