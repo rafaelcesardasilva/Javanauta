9b6876ec7442e7f94372b8eceb7a6d9de0d267c5
Rafael Cesar <rafael_planseq@hotmail.com>
2026-06-02 22:05:05 -0300

    Aprendendo sobre Expressões Comparativas

---
+++ Java/02-EstruturasCondicionais/src/ExpressoesComparativas.java
@@ -0,0 +1,18 @@
+/*
+OPERADORES COMPARATIVOS
+== !=  >  <  >=  <=
+
+ */
+public class ExpressoesComparativas {
+    static void main(String[] args) {
+        int pao = 10;
+        int acucar = 20;
+
+        System.out.println(pao == acucar);
+        System.out.println(pao != acucar);
+        System.out.println(pao > acucar);
+        System.out.println(pao < acucar);
+        System.out.println(pao >= acucar);
+        System.out.println(pao <= acucar);
+    }
+}
